4c159b0663b1ddc4d4419f0ce93a770ad6736e01
jaxishah <chawdajaxi@gmail.com>
2025-09-29 15:51:03 +0530

    check git action

--- playwright.config.js
+++ playwright.config.js
@@ -86,10 +86,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npx http-server . -p 3000',
+    port: 3000,
+    reuseExistingServer: !process.env.CI, // reuse locally
+  },
 });
 
